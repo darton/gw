c08d9b8ddaa91c3f2566e39b21563ca0937c2a2a
Dariusz Kowalczyk <darton@users.noreply.github.com>
2026-04-02 17:11:40 +0200

    Update SCRIPT_NAME assignment to use BASH_SOURCE

--- scripts/gw.sh
+++ scripts/gw.sh
@@ -14,7 +14,7 @@
 
 PATH=/sbin:/usr/sbin/:/bin:/usr/bin:$PATH
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-SCRIPT_NAME="$(basename "$0")"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
 GW_CONF_PATH="${SCRIPT_DIR}/gw.conf"
 GW_FUNCTIONS_PATH="$SCRIPT_DIR/gwfunctions.sh"
 
